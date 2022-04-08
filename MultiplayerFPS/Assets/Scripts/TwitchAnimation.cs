3b111fc76dde5f69c783e34ab6e284f37f21c3f2
AIE\s213321 <s213321@students.aie.edu.au>
2022-04-08 17:02:13 +0930

    Alien Organics Added, Obstacles and Cover Added, Lighting and Terrain added.

---
+++ MultiplayerFPS/Assets/Scripts/TwitchAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitchAnimation : MonoBehaviour
+{
+    Animator animator;
+    float timeToAnimEnd;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+
+        ChangeClip();
+    }
+
+
+    void ChangeClip()
+    {
+        Debug.Log("Playing");
+        int rand = Random.Range(0, 2);
+
+        animator.SetInteger("ClipNumber",rand);
+        
+        timeToAnimEnd = animator.GetCurrentAnimatorStateInfo(0).length;
+
+        Invoke("ChangeClip", timeToAnimEnd);
+    }
+}
